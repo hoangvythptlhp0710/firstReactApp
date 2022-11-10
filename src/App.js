a2ed8758b7244f11970bb16e6183f9bb474b46fd
hoangvytb7891 <hoangvytb7891@gmail.com>
2022-11-10 23:47:56 +0700

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Hello from React
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
